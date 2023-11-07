1c7c244137b121bb5ee9939e998217022c154b14
farhanshahriyar <farhan.abir9999@gmail.com>
2023-11-07 23:13:50 +0600

    added addJobs functionalities

--- src/pages/AddJobs/AddJobs.jsx
+++ src/pages/AddJobs/AddJobs.jsx
@@ -2,65 +2,108 @@
 import React, { useState } from 'react'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import Swal from 'sweetalert2';
 
 const AddJobs = () => {
     const [postingDate, setPostingDate] = useState(new Date());
 
     const jobPostHandler = (e) => {
         e.preventDefault();
-        console.log("Job Post e click porse mama");
-    };
+        // console.log("Job Post e click porse mama");
+        // get all data from the form
+        const form = e.target;
+        const banner = form.banner.value;
+        const cname = form.cname.value;
+        const uname = form.uname.value;
+        const category = form.category.value;
+        const salary = form.salary.value;
+        const description = form.description.value;
+        const date = form.date.value;
+
+        // passing info
+        const newJob = {
+            banner,
+            cname,
+            uname,
+            category,
+            salary,
+            description,
+            date
+        }
+        // console.log(newJob) //data passing successfully
+        fetch('http://localhost:5000/jobs',{
+          method: 'POST',
+          headers: {
+              'content-type': 'application/json'
+          },
+          body: JSON.stringify(newJob)
+      })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data)
+          if(data.insertedId){
+              // alert('Coffee added successfully')
+              Swal.fire({
+                title: 'Success!',
+                text: 'Coffee added successfully',
+                icon: 'success',
+                confirmButtonText: 'Done'
+              })
+          } else {
+              // alert('Something went wrong')
+              Swal.fire({
+                title: 'Error!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Back'
+              })
+          }
+      })
+  
+  
+      // clear the form
+      // form.reset()
+      }
+  
   return (
 
     <div className="max-w-4xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
       <form onSubmit={jobPostHandler}>
         <div className="bg-white rounded-xl shadow dark:bg-slate-900">
-          <div className="relative h-40 rounded-t-xl bg-[url('https://preline.co/assets/svg/component/abstract-bg-1.svg')] bg-no-repeat bg-cover bg-center">
-            <div className="absolute top-0 end-0 p-4">
-              <button type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                <svg className="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" ><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" x2="12" y1="3" y2="15"/></svg>
-                Upload Job Banner
-              </button>
-            </div>
-          </div>
-    
-          <div className="pt-0 p-4 sm:pt-0 sm:p-7">
+          <h2 className="text-lg font-medium text-gray-800 dark:text-gray-200 bg-white dark:bg-slate-900 px-4 py-5 sm:px-6 rounded-t-xl border-b border-gray-200 dark:border-gray-700">Add Jobs</h2>
+          <div className="mt-3 p-4 sm:pt-0 sm:p-7">
 
             <div className="space-y-4 sm:space-y-6">
-              <div>
-                <label className="sr-only">
-                  Profile Photo
+            <div className="space-y-2">
+                <label className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-gray-200">
+                  Job Banner Link
                 </label>
     
-                <div className="grid sm:flex sm:items-center sm:gap-x-5">
-                  <img className="-mt-8 relative z-10 inline-block h-24 w-24 mx-auto sm:mx-0 rounded-full ring-4 ring-white dark:ring-gray-800" src="../assets/img/160x160/img1.jpg" alt="Image Description"/>
-    
-                  <div className="mt-4 sm:mt-auto sm:mb-1.5 flex justify-center sm:justify-start gap-2">
-                    <button type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                      <svg className="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" x2="12" y1="3" y2="15"/></svg>
-                      Upload logo
-                    </button>
-                    <button type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-red-500 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                      Delete
-                    </button>
-                  </div>
-                </div>
+                <input id="af-submit-app-project-name" type="text" name='banner' className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Upload job banner link"/>
               </div>
-    
+           
               <div className="space-y-2">
                 <label className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-gray-200">
                   Job Title
                 </label>
     
-                <input id="af-submit-app-project-name" type="text" className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Enter Job Position Title. Example : Frontend Developer Intern"/>
+                <input id="af-submit-app-project-name" type="text" name='jobTitle' className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Enter Job Position Title. Example : Frontend Developer Intern"/>
+              </div>
+
+              <div className="space-y-2">
+                <label className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-gray-200">
+                  Company Name
+                </label>
+    
+                <input id="af-submit-app-project-name" type="text" name='cname' className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Company logo link"/>
               </div>
     
               <div className="space-y-2">
                 <label className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-gray-200">
-                   Logged in user name
+                   Logged in username
                 </label>
     
-                <input id="af-submit-project-url" type="text" className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Logged User. Example : MD.FARHAN SHAHRIYAR"/>
+                <input id="af-submit-project-url" type="text" name='uname' className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Logged User. Example : MD.FARHAN SHAHRIYAR"/>
               </div>
     
               <div className="space-y-2">
@@ -68,7 +111,7 @@ const AddJobs = () => {
                   Category
                 </label>
     
-                <select id="af-submit-app-category" className="py-2 px-3 pe-9 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600">
+                <select name='category' id="af-submit-app-category" className="py-2 px-3 pe-9 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600">
                   <option selected>Select a Job Category</option>
                   <option>On Site</option>
                   <option>Remote</option>
@@ -82,7 +125,7 @@ const AddJobs = () => {
                    Salary Range
                 </label>
     
-                <input id="af-submit-project-url" type="text" className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder=" $90k - $130k or $100k - $150k"/>
+                <input id="af-submit-project-url" type="text" name='salary' className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder=" $90k - $130k or $100k - $150k"/>
               </div>
             
                 <div className="space-y-2">
@@ -90,7 +133,7 @@ const AddJobs = () => {
                  Job Description
                 </label>
     
-                <textarea id="af-submit-app-description" className="py-2 px-3 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" rows="6" placeholder="A detailed summary will better explain your jobs to the audiences. Our users will see this in your dedicated jobs in page."></textarea>
+                <textarea id="af-submit-app-description" name='description' className="py-2 px-3 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" rows="6" placeholder="A detailed summary will better explain your jobs to the audiences. Our users will see this in your dedicated jobs in page."></textarea>
               </div>
 
               <div className="space-y-2">
@@ -98,6 +141,7 @@ const AddJobs = () => {
                     Job Posting Date
                 </label>
                 <DatePicker
+                    name='date'
                     selected={postingDate}
                     onChange={(date) => setPostingDate(date)}
                     className="py-2 px-3 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
